39e9e15a722b4509ee52457cb7e7e57e888c45d1
wideyedwonderer <codisanko@gmail.com>
2021-01-27 17:11:32 +0200

    console.log

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -14,7 +14,6 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    console.log( action)
     if(action.type === ACTION_TYPE.TOGGLE_SIDEBAR) {
         return {
             ...state,
