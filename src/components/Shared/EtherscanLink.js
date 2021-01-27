df1ada66f0f0099224305d2ac5b77a398bd8c6b4
wideyedwonderer <codisanko@gmail.com>
2021-01-27 17:27:08 +0200

    etherscan link

--- src/components/Shared/EtherscanLink.js
+++ src/components/Shared/EtherscanLink.js
@@ -1,15 +1,17 @@
 import React, {useContext} from 'react';
 import loading from '../../assets/images/loading.svg';
 import AppContext from '../../ContextAPI';
+import './EtherscanLink.css';
 
 const EtherscanLink = (props) => {
 
     const {connectedNetwork} = useContext(AppContext);
     const link = `https://${connectedNetwork ? connectedNetwork + '.': ""}etherscan.io/tx/${props.txId}`
     return (
-        <div>
-            <a href={link}>See on Etherscan</a>
-            <img src={loading} alt='Loading...' />
+        <div className="etherscanContainer">
+
+              <a href={link} className='etherscanLink'>See on Etherscan</a>
+            <img src={loading} alt='Loading...' className="etherscanImage"/>
         </div>
     )
 }
